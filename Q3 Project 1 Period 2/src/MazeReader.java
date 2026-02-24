a49b40944f51ed47767c351011ac56763f295e60
1952797 <1952797@01-18129.pusd.dom>
2026-02-24 11:55:18 -0800

    add arg passing and printMap

--- Q3 Project 1 Period 2/src/MazeReader.java
+++ Q3 Project 1 Period 2/src/MazeReader.java
@@ -5,11 +5,50 @@ import java.util.Scanner;
 
 public class MazeReader {
     public static void main(String[] args) throws FileNotFoundException {
-        String[][][] map = getMap("src/easyMap1.txt");
+        String[][][] map = getTextBasedMap("src/easyMap1.txt");
+        printMap(map);
+
+
+        boolean queueBase = false;
+        boolean stackBase = false;
+        boolean optimal = false;
+        boolean showTime = false;
+        boolean inCoord = false;
+        boolean outCoord = false;
+
+
+        for(String arg : args) {
+            if(arg.equals("--Stack")) {
+                stackBase = true;
+            }
+            if(arg.equals("--Queue")) {
+                queueBase = true;
+            }
+            if(arg.equals("--Opt")) {
+                optimal = true;
+            }
+            if(arg.equals("--Time")) {
+                showTime = true;
+            }
+            if(arg.equals("--Incoordinate")) {
+                inCoord = true;
+            }
+            if(arg.equals("--Outcoordinate")) {
+                outCoord = true;
+            }
+            if(arg.equals("--Help")) {
+                System.out.println("Help");
+            }
+        }
+
+
+        if(stackBase == true && queueBase == true) {
+            System.exit(-1);
+        }
     }
 
 
-    public static String[][][] getMap(String fileName) throws FileNotFoundException {
+    public static String[][][] getTextBasedMap(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         Scanner scanner = new Scanner(file);
 
@@ -34,6 +73,18 @@ public class MazeReader {
 
         scanner.close();
         return mapping;
+    }
+
+
+    public static void printMap(String[][][] map) {
+        for(int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map[i].length; j++) {
+                for(int h = 0; h < map[i][j].length; h++) {
+                    System.out.print(map[i][j][h]);
+                }
+                System.out.println();
+            }
+        }
     }
 }
 
